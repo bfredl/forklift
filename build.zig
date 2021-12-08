cfd2d6cb02597b38fe08d2d242e98da9ae264f79
Björn Linse <bjorn.linse@gmail.com>
2021-12-08 18:21:41 +0100

    initial commit

---
+++ build.zig
@@ -0,0 +1,12 @@
+const Builder = @import("std").build.Builder;
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+    var exe = b.addExecutable("zigbeam", "src/main.zig");
+    exe.setBuildMode(mode);
+    exe.install();
+
+    const operate = b.step("operate", "operate the forklift");
+    const run = exe.run();
+    run.step.dependOn(b.getInstallStep());
+    operate.dependOn(&run.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() !void {
+    print("Yes, I am your CFO (certified forklift operator)\n", .{});
+}
