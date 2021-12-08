9da9d1662244a49cf6688539bdaf998463c7b588
Björn Linse <bjorn.linse@gmail.com>
2021-12-08 22:54:08 +0100

    testcases

---
+++ testcases.asm
@@ -0,0 +1,14 @@
+global main
+    section   .text
+main:
+
+    mov rax, rdi
+    mov eax, edi
+    mov ax, di
+    mov al, dil
+    mov al, bh
+
+    mov rax, rbx
+    mov eax, ebx
+    mov ax, bx
+    mov al, bl
