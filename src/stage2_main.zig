8abf4098c48ce4068574b1b4b79d16c3431dade1
Björn Linse <bjorn.linse@gmail.com>
2021-12-22 13:33:45 +0100

    here

--- src/stage2_main.zig
+++ src/stage2_main.zig
@@ -8,11 +8,14 @@ pub fn main2() !void {
     // var cfo = CFO.init(allocator) catch unreachable;
     var cfo = CFO.init_stage2();
     var pos = cfo.get_target();
+    try cfo.rex_wrxb(true, false, false, false);
+    try cfo.wb(0x8b); // OP reg, \rm
+    try cfo.modRm(0b11, 0, 6);
     try cfo.ret();
     try cfo.finalize_stage2();
 
-    var pointer = cfo.get_ptr_stage2(pos, fn () callconv(.C) void);
-    pointer();
+    var fun = cfo.get_ptr_stage2(pos, fn (usize, usize) callconv(.C) usize);
+    std.os.exit(@intCast(u8, fun(10, 5)));
 }
 
 pub fn main() void {
