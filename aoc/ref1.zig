49db0b7e7cf4ca8a1d5bd7077d07156fe126d9af
bfredl <bjorn.linse@gmail.com>
2023-01-20 15:17:43 +0100

    dott

---
+++ aoc/ref1.zig
@@ -0,0 +1,25 @@
+export fn doit(data: [*]u8, len: usize) callconv(.C) isize {
+    var max: isize = 0;
+    var ipos: usize = 0;
+    while (ipos < len) { // fyy
+        var sum: isize = 0;
+        while (ipos < len) {
+            var item: isize = 0;
+            const prebyte: isize = data[ipos];
+            const pretoken: isize = prebyte - 48;
+            if (pretoken < 0 or pretoken > 9) break;
+            while (ipos < len) { // fyyy
+                const byte: isize = data[ipos];
+                const token: isize = byte - 48;
+                if (token < 0 or token > 9) break;
+                item = 10 * item + token;
+                ipos += 1;
+            }
+            sum += item;
+            ipos += 1;
+        }
+        ipos += 1;
+        if (sum > max) max = sum;
+    }
+    return max;
+}
