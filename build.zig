1ba57d14261f0351ff7d6071baf702add464df13
bfredl <bjorn.linse@gmail.com>
2023-02-01 18:00:11 +0100

    my plate has been deleted

--- build.zig
+++ build.zig
@@ -1,29 +1,33 @@
-const Builder = @import("std").build.Builder;
-pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    var exe = b.addExecutable("operator", "src/main.zig");
-    exe.setBuildMode(mode);
+const std = @import("std");
+pub fn build(b: *std.Build) void {
+    const opt = b.standardOptimizeOption(.{});
+    var exe = b.addExecutable(.{
+        .name = "operator",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .optimize = opt,
+    });
     exe.install();
 
-    var exe_parse = b.addExecutable("parse", "src/parse.zig");
-    exe_parse.setBuildMode(mode);
-    exe_parse.install();
-
     const ir = b.step("ir", "run some ir");
-    var exe_ir = b.addExecutable("run", "src/run_ir.zig");
-    exe_ir.setBuildMode(mode);
+    var exe_ir = b.addExecutable(.{
+        .name = "run",
+        .root_source_file = .{ .path = "src/run_ir.zig" },
+        .optimize = opt,
+    });
     exe_ir.install();
     const install_ir = b.addInstallArtifact(exe_ir);
     ir.dependOn(&install_ir.step);
 
-    const operate = b.step("operate", "operate the forklift");
-    const run = exe.run();
-    run.step.dependOn(b.getInstallStep());
-    operate.dependOn(&run.step);
-
     if (false) {
+        var exe_parse = b.addExecutable("parse", "src/parse.zig");
+        exe_parse.install();
+
+        const operate = b.step("operate", "operate the forklift");
+        const run = exe.run();
+        run.step.dependOn(b.getInstallStep());
+        operate.dependOn(&run.step);
+
         var exe_looptest = b.addExecutable("loop_test", "src/loop_test.zig");
-        exe_looptest.setBuildMode(mode);
         exe_looptest.install();
 
         const loop_test = b.step("loop_test", "loop_test the forklift");
