2d7f3f128e95a13fff3ddb0ced6f9febc8f35a1c
bfredl <bjorn.linse@gmail.com>
2025-12-26 10:06:50 +0100

    LOVE WILL (NOT) TEAR US APART

--- test/call_ret.zig
+++ test/call_ret.zig
@@ -15,35 +15,63 @@ fn target_int8(a: usize, b: usize, c: usize, d: usize, e: usize, f: usize, g: us
     res = .{ .a = a, .b = b, .c = c, .d = d, .e = e, .f = f, .g = g, .h = h };
 }
 
-test "basic" {
+const ArgSpec = union(enum) { arg: usize, val: usize };
+fn p(arg: usize) ArgSpec {
+    return .{ .arg = arg };
+}
+fn v(val: usize) ArgSpec {
+    return .{ .val = val };
+}
+
+fn fluffer(mod: *CFOModule, narg: usize, spec: []const ArgSpec) !EightBallFn {
     var self = try FLIR.init(8, test_allocator);
     defer self.deinit();
-
     const start = try self.addNode();
 
-    const CANTER = 6; // TODO: make me eight!!!
+    const CANTER = 8;
+    if (narg > CANTER) return error.FixThis;
 
-    var args: [8]u16 = @splat(FLIR.NoRef);
-    for (0..CANTER) |i| {
+    var args: [CANTER]u16 = @splat(FLIR.NoRef);
+    for (0..narg) |i| {
         args[i] = try self.arg(.{ .intptr = .quadword });
     }
 
     const call = try self.call(start, .fun_addr, try self.const_uint(@intFromPtr(&target_int8)));
     var arglist = call;
-    for (0..CANTER) |i| {
-        arglist = try self.callarg(start, arglist, args[i], .{ .intptr = .quadword });
+    for (spec) |s| {
+        const val = switch (s) {
+            .arg => |ia| if (ia < narg) args[ia] else return error.CanTFixThis,
+            .val => |va| try self.const_uint(va),
+        };
+
+        arglist = try self.callarg(start, arglist, val, .{ .intptr = .quadword });
     }
 
     try self.ret(start);
-    self.debug_print();
     try self.test_analysis(FLIR.X86ABI, true);
     self.debug_print();
 
-    var mod = try CFOModule.init(test_allocator);
-    defer mod.deinit_mem();
-    const target = try codegen(&self, &mod, false, null);
+    const target = try codegen(&self, mod, false, null);
     try mod.code.finalize();
 
     const fun = mod.code.get_ptr(target, EightBallFn);
+    return fun;
+}
+
+test "basic" {
+    var mod = try CFOModule.init(test_allocator);
+    defer mod.deinit_mem();
+
+    const fun = try fluffer(&mod, 6, &.{ p(0), p(1), p(2), p(3), p(4), p(5) });
+
+    fun(10, 11, 12, 13, 14, 15, 16, 17);
+}
+
+test "shift in" {
+    var mod = try CFOModule.init(test_allocator);
+    defer mod.deinit_mem();
+
+    const fun = try fluffer(&mod, 6, &.{ v(67), p(0), p(1), p(2), p(3), p(4) });
+
     fun(10, 11, 12, 13, 14, 15, 16, 17);
 }
