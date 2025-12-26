496de99f81695308b1373d691f3c6777e24c46ab
bfredl <bjorn.linse@gmail.com>
2025-12-26 11:11:04 +0100

    KLUBBBER

--- test/call_ret.zig
+++ test/call_ret.zig
@@ -8,11 +8,11 @@ const codegen = forklift.codegen_x86_64;
 const std = @import("std");
 const test_allocator = std.testing.allocator;
 
-var res: struct { a: usize, b: usize, c: usize, d: usize, e: usize, f: usize, g: usize, h: usize } = undefined;
+var res: [8]usize = undefined;
 
 const EightBallFn = *const fn (a: usize, b: usize, c: usize, d: usize, e: usize, f: usize, g: usize, h: usize) callconv(.c) void;
 fn target_int8(a: usize, b: usize, c: usize, d: usize, e: usize, f: usize, g: usize, h: usize) callconv(.c) void {
-    res = .{ .a = a, .b = b, .c = c, .d = d, .e = e, .f = f, .g = g, .h = h };
+    res = .{ a, b, c, d, e, f, g, h };
 }
 
 const ArgSpec = union(enum) { arg: usize, val: usize };
@@ -23,13 +23,16 @@ fn v(val: usize) ArgSpec {
     return .{ .val = val };
 }
 
-fn fluffer(mod: *CFOModule, narg: usize, spec: []const ArgSpec) !EightBallFn {
+fn fluffer(narg: usize, spec: []const ArgSpec) !void {
+    var mod = try CFOModule.init(test_allocator);
+    defer mod.deinit_mem();
+
     var self = try FLIR.init(8, test_allocator);
     defer self.deinit();
     const start = try self.addNode();
 
     const CANTER = 8;
-    if (narg > CANTER) return error.FixThis;
+    if (narg > CANTER or spec.len > CANTER) return error.FixThis;
 
     var args: [CANTER]u16 = @splat(FLIR.NoRef);
     for (0..narg) |i| {
@@ -49,29 +52,40 @@ fn fluffer(mod: *CFOModule, narg: usize, spec: []const ArgSpec) !EightBallFn {
 
     try self.ret(start);
     try self.test_analysis(FLIR.X86ABI, true);
-    self.debug_print();
+    // self.debug_print();
 
-    const target = try codegen(&self, mod, false, null);
+    const target = try codegen(&self, &mod, false, null);
     try mod.code.finalize();
 
     const fun = mod.code.get_ptr(target, EightBallFn);
-    return fun;
+    fun(10, 11, 12, 13, 14, 15, 16, 17);
+
+    const n = spec.len;
+    var expect: [CANTER]usize = undefined;
+    for (expect[0..n], spec) |*e, s| {
+        e.* = switch (s) {
+            .arg => |ia| 10 + ia,
+            .val => |va| va,
+        };
+    }
+
+    try std.testing.expectEqualSlices(usize, expect[0..n], res[0..n]);
 }
 
 test "basic" {
-    var mod = try CFOModule.init(test_allocator);
-    defer mod.deinit_mem();
-
-    const fun = try fluffer(&mod, 6, &.{ p(0), p(1), p(2), p(3), p(4), p(5) });
-
-    fun(10, 11, 12, 13, 14, 15, 16, 17);
+    try fluffer(6, &.{ p(0), p(1), p(2), p(3), p(4), p(5) });
 }
 
 test "shift in" {
-    var mod = try CFOModule.init(test_allocator);
-    defer mod.deinit_mem();
+    try fluffer(6, &.{ v(67), p(0), p(1), p(2), p(3), p(4) });
+    try fluffer(6, &.{ v(1001), v(1010), p(0), p(1), p(2), p(3) });
+}
 
-    const fun = try fluffer(&mod, 6, &.{ v(67), p(0), p(1), p(2), p(3), p(4) });
+test "swap 2" {
+    try fluffer(6, &.{ p(1), p(0), p(2), p(3), p(4), p(5) });
+}
 
-    fun(10, 11, 12, 13, 14, 15, 16, 17);
+test "swap 3" {
+    try fluffer(6, &.{ p(2), p(0), p(1), p(3), p(4), p(5) });
+    try fluffer(6, &.{ p(1), p(2), p(0), p(3), p(4), p(5) });
 }
